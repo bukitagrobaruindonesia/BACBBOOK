d20746ecc7ee30d6c109e9d6f5af5b3eea52dcf9
bukitagrobaruindonesia <bukitagrochemicalbaru@gmail.com>
2026-06-24 14:13:32 +0700

    update config js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,31 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' blob: data: https://*.googleapis.com https://*.firebaseio.com",
+              "font-src 'self'",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+              "frame-ancestors 'none'",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://api.emailjs.com",
+              "upgrade-insecure-requests",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
